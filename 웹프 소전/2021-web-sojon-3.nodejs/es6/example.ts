06090bfb9ace4d6a23cc9824eea4d98ddfb0b3dc
andan04 <andan04@naver.com>
2021-06-15 12:11:46 +0900

    typescript translate

---
+++ 웹프 소전/2021-web-sojon-3.nodejs/es6/example.ts
@@ -0,0 +1,9 @@
+import _ from "lodash";
+const arr: number[] = _.fill(Array(10), 4)
+console.log(arr);
+import * as logger from "./logger";
+logger.debug("This is debug!");
+logger.log("This is log!!");
+logger.error("This is error!!!");
+import calc from "./calcurator"
+console.log(calc.sum(3, 4), calc.sub(4, 3), calc.multiply(2, 2), calc.divide(3, 2));
---
+++ 웹프 소전/2021-web-sojon-3.nodejs/es6/logger.ts
@@ -0,0 +1,14 @@
+export function debug(msg: string): void
+{
+    console.debug(msg);
+}
+
+export function log(msg: string): void
+{
+    console.log(msg);
+}
+
+export function error(msg: string): void
+{
+    console.error(msg)
+}
